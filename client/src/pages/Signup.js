536cef4948221fd5c330ee1e5de206abd90d7c6e
kvammee <elliott.kvamme@gmail.com>
2022-01-19 16:50:23 -0600

    Added functionality to the Signup and Login forms

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
 
 const Signup = () => {
   const [formState, setFormState] = useState({ username: '', email: '', password: '' });
+
+  const [addUser, { error }] = useMutation(ADD_USER);
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -13,9 +17,19 @@ const Signup = () => {
     });
   };
 
-  // submit form
+  // submit form (Asynchronous)
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+    // use try/catch instead of Promises
+    try {
+      // execute addUser mutation and pass in variable data from form
+      const { data } = await addUser({
+        variables: { ...formState }
+      });
+      console.log(data);
+    } catch(e) {
+      console.log(e);
+    }
   };
 
   return (
@@ -56,6 +70,7 @@ const Signup = () => {
                 Submit
               </button>
             </form>
+            {error && <div>Sign up failed</div>}
           </div>
         </div>
       </div>
